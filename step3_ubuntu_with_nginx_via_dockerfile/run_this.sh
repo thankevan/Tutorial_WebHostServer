69c2b9de0adc0ac70779d72ebda835c5113d51da
thankevan <thankevan@gmail.com>
2024-02-15 16:53:39 -0500

    Update run_this.sh

--- step3_ubuntu_with_nginx_via_dockerfile/run_this.sh
+++ step3_ubuntu_with_nginx_via_dockerfile/run_this.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# step2_nginx_with_attached_content
+# step3_ubuntu_with_nginx_via_dockerfile
+
+# Build the image using the docker file in the current folder
+# `-t` takes `name[:tag]` for the image. Tag defaults to ":latest" if not given.
+docker build -t ubuntu_with_nginx_via_dockerfile_image .
 
 echo "---"
-echo "After nginx boots you can see the custom content if you navigate to:"
+echo "After nginx starts you can see the custom content if you navigate to:"
 echo "  http://localhost:8000"
 
 # Execute a shell on the container - allowing you to operate on it
 # This effectively logs in as root.
 #
 # Breakdown:
-#   `docker exec ... nginx_with_attached_content_container sh`: Execute the sh
-#         command on the container nginx_with_attached_content_container.
+#   `docker exec ... ubuntu_with_nginx_via_dockerfile_container bash`: Execute
+#         the bash command on the specified container.
 #     `-i`: interactive flag. Let what I type go to the container.
 #     `-t`: pseudo-tty flag. Do a bunch of shell-like things such as give you a
 #           prompt, print more than one result on a line for commands like ls,
@@ -23,24 +27,22 @@ echo "  http://localhost:8000"
 #         input in and the other effectively renders/formats the output.
 echo "---"
 echo "To get on the running container after nginx boots:"
-echo "  docker exec -it nginx_with_attached_content_container sh"
+echo "  docker exec -it ubuntu_with_nginx_via_dockerfile_container bash"
 
 echo "---"
 echo "To stop the running container: CONTROL-C"
 
-# When you hit CONTROL-C to exit the container, continue running the script
-trap INT
-
-# Run container based on nginx:latest image
+# Run container based on ubuntu_with_nginx_via_dockerfile_image:latest image
 #
 # Breakdown:
-#   `docker run ... nginx`: Create and start a container based on the
-#         nginx:latest image.
+#   `docker run ... ubuntu_with_nginx_via_dockerfile_image`: Create and start a
+#         container based on the ubuntu_with_nginx_via_dockerfile_image:latest
+#         image.
 #     `-p 8000:80`: Tunnel my port 8000 to the container's port 80. Containers
 #           run in their own docker network which is not directly accessible.
 #     `--name nginx_with_attached_content_container`: name the container to make
 #           it easy to restart it or run commands in it.
-#     `-v ./nginx_html:/usr/share/nginx/html`: use the volume flag to create or
+#     `-v ./nginx_html:/var/www/html`: use the volume flag to create or
 #           overwrite the container folder on the right with the local folder on
 #           the left.
 #
@@ -51,15 +53,21 @@ trap INT
 #   - The image will be pulled from docker hub automatically.
 #
 echo "---"
-docker run -p 8000:80 --name nginx_with_attached_content_container -v ./nginx_html:/usr/share/nginx/html nginx
+docker run -p 8000:80 --name ubuntu_with_nginx_via_dockerfile_container \
+    -v ./nginx_html:/var/www/html ubuntu_with_nginx_via_dockerfile_image
 
 echo "---"
 echo "CLEAN UP"
 
 echo "---"
 echo "To remove the container run:"
-echo "  docker rm nginx_with_attached_content_container"
+echo "  docker rm ubuntu_with_nginx_via_dockerfile_container"
 
 echo "---"
 echo "To remove the image run:"
-echo "  docker rmi nginx:latest"
+echo "  docker rmi ubuntu_with_nginx_via_dockerfile_image:latest"
+
+echo "---"
+echo "To remove any cached objects (including containers and images) run:"
+echo "  docker system prune -f -a"
+
